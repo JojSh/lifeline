bcbf7f7f79e4a9825448d133145825f8c488940f
Joseph Sharratt <joj@Josephs-MacBook-Pro.local>
2017-09-20 18:34:59 +0100

    create user stories and initial readme

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Welcome to Lifeline</h2>
+          <h4>A visual map of your life</h4>
+        </div>
+        <p className='App-body'>
+
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ stories.md
@@ -0,0 +1,6 @@
+as a user
+I can see a visual representation of the current year
+
+I can set the year of my birth and see all of the years since
+
+I can add a named 'era' and set it's duration and colour
